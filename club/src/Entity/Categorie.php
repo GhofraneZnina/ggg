3829427c928b7fa92e1011970e9ded1cf5cf8ba7
siwar <siwar.maiti>
2023-02-23 09:47:16 +0100

    Relation

--- club/src/Entity/Categorie.php
+++ club/src/Entity/Categorie.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CategorieRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CategorieRepository::class)]
@@ -21,6 +23,14 @@ class Categorie
 
     #[ORM\Column(length: 255)]
     private ?string $intitule = null;
+
+    #[ORM\ManyToMany(targetEntity: Nageur::class, mappedBy: 'categorie')]
+    private Collection $nageurs;
+
+    public function __construct()
+    {
+        $this->nageurs = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -59,6 +69,33 @@ class Categorie
     public function setIntitule(string $intitule): self
     {
         $this->intitule = $intitule;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Nageur>
+     */
+    public function getNageurs(): Collection
+    {
+        return $this->nageurs;
+    }
+
+    public function addNageur(Nageur $nageur): self
+    {
+        if (!$this->nageurs->contains($nageur)) {
+            $this->nageurs->add($nageur);
+            $nageur->addCategorie($this);
+        }
+
+        return $this;
+    }
+
+    public function removeNageur(Nageur $nageur): self
+    {
+        if ($this->nageurs->removeElement($nageur)) {
+            $nageur->removeCategorie($this);
+        }
 
         return $this;
     }
