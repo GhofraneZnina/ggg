ab271368d4f5d030a5305702b759e8fee15b95ba
GhofraneZnina <ghofrane.znina@etudiant-isi.utm.tn>
2023-02-24 09:08:46 +0100

    register avant entité user

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,12 +18,18 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('login')
+           // ->add('login')
+           ->add('login',TextType::class, [
+            'attr' => [
+                'class' => 'form-control'
+            ],
+            'label' => 'Login'
+        ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'En m\'inscrivant à ce site j\'accepte',
                     ]),
                 ],
             ])
