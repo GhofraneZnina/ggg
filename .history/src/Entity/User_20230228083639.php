4968d0d5e09a1ac26d5cf985224c78d9e6f2c8bf
GhofraneZnina <ghofrane.znina@etudiant-isi.utm.tn>
2023-02-28 09:26:25 +0100

    register erreur

---
+++ .history/src/Entity/User_20230228083639.php
@@ -0,0 +1,243 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity(fields: ['login'], message: 'There is already an account with this login')]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    protected $id = 'id';
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 180, unique: true)]
+    private ?string $login = null;
+
+    #[ORM\Column]
+    private array $roles = [];
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
+
+     #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $prenom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $telephone = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $profilFacebook = null;
+
+    #[ORM\OneToOne(mappedBy: 'user', cascade: ['persist', 'remove'])]
+    private ?Nageur $nageur = null;
+
+    #[ORM\OneToOne(mappedBy: 'user', cascade: ['persist', 'remove'])]
+    private ?Parents $parents = null;
+
+    #[ORM\OneToOne(mappedBy: 'user', cascade: ['persist', 'remove'])]
+    private ?Entraineur $entraineur = null;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLogin(): ?string
+    {
+        return $this->login;
+    }
+
+    public function setLogin(string $login): self
+    {
+        $this->login = $login;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->login;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getTelephone(): ?string
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone(string $telephone): self
+    {
+        $this->telephone = $telephone;
+
+        return $this;
+    }
+
+    public function getProfilFacebook(): ?string
+    {
+        return $this->profilFacebook;
+    }
+
+    public function setProfilFacebook(string $profilFacebook): self
+    {
+        $this->profilFacebook = $profilFacebook;
+
+        return $this;
+    }
+
+    public function getNageur(): ?Nageur
+    {
+        return $this->nageur;
+    }
+
+    public function setNageur(Nageur $nageur): self
+    {
+        // set the owning side of the relation if necessary
+        if ($nageur->getUser() !== $this) {
+            $nageur->setUser($this);
+        }
+
+        $this->nageur = $nageur;
+
+        return $this;
+    }
+
+    public function getParents(): ?Parents
+    {
+        return $this->parents;
+    }
+
+    public function setParents(Parents $parents): self
+    {
+        // set the owning side of the relation if necessary
+        if ($parents->getUser() !== $this) {
+            $parents->setUser($this);
+        }
+
+        $this->parents = $parents;
+
+        return $this;
+    }
+
+    public function getEntraineur(): ?Entraineur
+    {
+        return $this->entraineur;
+    }
+
+    public function setEntraineur(Entraineur $entraineur): self
+    {
+        // set the owning side of the relation if necessary
+        if ($entraineur->getUser() !== $this) {
+            $entraineur->setUser($this);
+        }
+
+        $this->entraineur = $entraineur;
+
+        return $this;
+    }
+
+    
+
+   
+}
