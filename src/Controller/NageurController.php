f974e563f2d2ee2c4690d688b0a1aff0a31d6100
siwar <siwar.maiti>
2023-02-28 09:49:18 +0100

    controller

---
+++ src/Controller/NageurController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NageurController extends AbstractController
+{
+    #[Route('/nageur', name: 'app_nageur')]
+    public function index(): Response
+    {
+        return $this->render('nageur/index.html.twig', [
+            'controller_name' => 'NageurController',
+        ]);
+    }
+}
