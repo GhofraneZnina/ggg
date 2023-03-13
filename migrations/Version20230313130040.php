036c675e171dc812eb57b54d778ef7889922f91e
siwar <siwar.maiti>
2023-03-13 15:30:08 +0100

    suite listing

---
+++ migrations/Version20230313130040.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230313130040 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE physionomie (id INT AUTO_INCREMENT NOT NULL, nageur_id INT NOT NULL, date DATE NOT NULL, taille DOUBLE PRECISION NOT NULL, poids DOUBLE PRECISION NOT NULL, INDEX IDX_4269C3EC11C519B5 (nageur_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE physionomie ADD CONSTRAINT FK_4269C3EC11C519B5 FOREIGN KEY (nageur_id) REFERENCES nageur (id)');
+        $this->addSql('ALTER TABLE nageur DROP FOREIGN KEY FK_23C0CAA67A45358C');
+        $this->addSql('DROP INDEX IDX_23C0CAA67A45358C ON nageur');
+        $this->addSql('ALTER TABLE nageur DROP groupe_id');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE physionomie DROP FOREIGN KEY FK_4269C3EC11C519B5');
+        $this->addSql('DROP TABLE physionomie');
+        $this->addSql('ALTER TABLE nageur ADD groupe_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE nageur ADD CONSTRAINT FK_23C0CAA67A45358C FOREIGN KEY (groupe_id) REFERENCES groupe (id)');
+        $this->addSql('CREATE INDEX IDX_23C0CAA67A45358C ON nageur (groupe_id)');
+    }
+}
