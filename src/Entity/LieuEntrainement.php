c85e2ec6f076238174d16eb987bcba93341da8b3
siwar <siwar.maiti>
2023-03-31 04:20:00 +0200

    form et controller

--- src/Entity/LieuEntrainement.php
+++ src/Entity/LieuEntrainement.php
@@ -3,11 +3,19 @@
 namespace App\Entity;
 
 use App\Repository\LieuEntrainementRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Symfony\Component\Validator\Constraints as Assert;
+
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: LieuEntrainementRepository::class)]
 class LieuEntrainement
 {
+    const PICINE_1 = 'p25m' ;
+    const PICINE_2 ='p50' ;
+    const AUTRE='autre';
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
@@ -18,6 +26,10 @@ class LieuEntrainement
 
     #[ORM\Column(length: 255)]
     private ?string $description = null;
+    #[ORM\Column(type:'string')]
+    #[Assert\NotBlank]
+    #[Assert\Choice([self::PICINE_1, self::PICINE_2, self::AUTRE ])]
+    private ?string $typePicine;
 
     public function getId(): ?int
     {
@@ -47,4 +59,16 @@ class LieuEntrainement
 
         return $this;
     }
-}
+        public function getTypePicine(): ?string
+    {
+        return $this->typePicine;
+
+    }
+    public function setTypePicine(string $typePicine): self
+    {
+        $this->typePicine = $typePicine;
+
+        return $this;
+    }
+    }
+
