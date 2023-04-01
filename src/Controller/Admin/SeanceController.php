d201661d256c914d146fb90b6eef3a922f2519e1
siwar <siwar.maiti>
2023-04-01 02:14:48 +0200

    listing

--- src/Controller/Admin/SeanceController.php
+++ src/Controller/Admin/SeanceController.php
@@ -1,18 +1,82 @@
 <?php
 
-namespace App\Controller;
-
+namespace App\Controller\Admin;
+use App\Entity\Seance;
+use App\Form\Admin\SeanceType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SeanceController extends AbstractController
 {
-    #[Route('/seance', name: 'app_seance')]
-    public function index(): Response
-    {
-        return $this->render('seance/index.html.twig', [
-            'controller_name' => 'SeanceController',
-        ]);
+
+    public function __construct(private EntityManagerInterface $em) {
+        ;
     }
+
+
+    #[Route('/admin/seance', name: 'app_admin_seance_list')]
+    public function index(Request $request): Response
+    {
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('login') ;
+     }
+
+    //create 
+    $seance = new Seance() ;
+    $form = $this->createForm(SeanceType::class, $seance);
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+
+ 
+         $this->em->persist($seance);
+         $this->em->flush();
+
+        $this->addFlash('success','seance successfully created' );
+
+        return $this->redirectToRoute('app_admin_seance_list') ;
+    } else if ($form->isSubmitted() && !$form->isValid()) {
+
+       //dd($form->getData());
+        $this->addFlash('error','check your data');
+     }
+     //  TODO : create new seance : END 
+     
+    $seance = $this->em->getRepository(Seance::class)->findAll() ;
+     return $this->render('admin/seance/index.html.twig', [
+        'form' => $form->createView(),
+        'seance' => $seance,
+     ]);
+
+
+
+
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
 }
