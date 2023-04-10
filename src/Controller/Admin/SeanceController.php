7cd27d472cdfefd4247b25a828f8fb6c39a1b35d
GhofraneZnina <ghofrane.znina@etudiant-isi.utm.tn>
2023-04-10 10:30:29 +0100

    some changes

--- src/Controller/Admin/SeanceController.php
+++ src/Controller/Admin/SeanceController.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 class SeanceController extends AbstractController
 {
 
