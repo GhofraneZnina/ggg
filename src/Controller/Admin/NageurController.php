85318c81cee49ef15e1050c5a5221e0a9d7510e2
GhofraneZnina <ghofrane.znina@etudiant-isi.utm.tn>
2023-04-25 09:03:07 +0100

    nageur planning

--- src/Controller/Admin/NageurController.php
+++ src/Controller/Admin/NageurController.php
@@ -290,55 +290,46 @@ class NageurController extends AbstractController
 
 
             //listing nageur
-        $nageurs = $this->em->getRepository(Nageur::class)->findOneBy(['id' => $id]);
-        $seance = $this->em->getRepository(Seance::class)->getSeance($nageur);
-        dd($seance);
-        $seance = $this->em->createQueryBuilder()
-        ->select('s')
-        ->from('App\Entity\Seance', 's')
-        ->join('s.groupe', 'g')
-        ->join('g.nageur', 'e')
-        ->join('s.planning', 'p')
-        ->where('e.id = :nageurId')
-        ->andWhere('s.planning IS NOT NULL')
-        ->andWhere('p.status = 1')
-        ->andWhere('g.id = :groupeId')
-        ->setParameter('nageurId', $nageurs->getId())
-        ->setParameter('groupeId', $nageurs->getGroupe()->getId())
-        ->getQuery()
-        ->getResult();
-        //dd($seance);
-        if (!$nageurs) {
-            return $this->redirectToRoute('app_admin_nageur_page');
-         }
-         //nageur seance
-         $seancesByDay = [];
-
-         // Check if the Nageur is assigned to a Groupe
-         if ($nageurs->getGroupe() !== null) {
-             // Get the Seances for the Groupe
-             foreach ($nageurs->getGroupe()->getSeances() as $seance) {
-                 $dayOfWeek = $seance->getJour();
-                 if (!isset($seancesByDay[$dayOfWeek])) {
-                     $seancesByDay[$dayOfWeek] = [];
-                 }
-                 $seancesByDay[$dayOfWeek][] = $seance;
-             }
-         }
-         //
-
-        return $this->render('admin/nageur/pageNageur.html.twig', [
-        'nageurs' => $nageurs,
-        'form' => $form->createView(),
-        'formEdit' => $formEdit->createView(),
-        'formPhysionomie' => $formPhysionomie->createView(),
-        'seanceByDay'=>$seancesByDay,
-        'seance'=>$seance,
-
-        ]);
-
-              
-    }
+            $nageurs = $this->em->getRepository(Nageur::class)->findOneBy(['id' => $id]);
+    
+            // Retrieve all the Seance entities associated with the given Nageur entity
+            $seances = $this->em->createQueryBuilder()
+                ->select('s')
+                ->from('App\Entity\Seance', 's')
+                ->join('s.groupe', 'g')
+                ->join('g.nageur', 'e')
+                ->join('s.planning', 'p')
+                ->where('e.id = :nageurId')
+                ->andWhere('s.planning IS NOT NULL')
+                ->andWhere('p.status = 1')
+                ->andWhere('g.id = :groupeId')
+                ->setParameter('nageurId', $nageurs->getId())
+                ->setParameter('groupeId', $nageurs->getGroupe()->getId())
+                ->getQuery()
+                ->getResult();
+        
+            // Check if the Nageur is assigned to a Groupe
+            $seancesByDay = [];
+            if ($nageurs->getGroupe() !== null) {
+                // Get the Seances for the Groupe
+                foreach ($nageurs->getGroupe()->getSeances() as $seance) {
+                    $dayOfWeek = $seance->getJour();
+                    if (!isset($seancesByDay[$dayOfWeek])) {
+                        $seancesByDay[$dayOfWeek] = [];
+                    }
+                    $seancesByDay[$dayOfWeek][] = $seance;
+                }
+            }
+        
+            return $this->render('admin/nageur/pageNageur.html.twig', [
+                'nageurs' => $nageurs,
+                'form' => $form->createView(),
+                'formEdit' => $formEdit->createView(),
+                'formPhysionomie' => $formPhysionomie->createView(),
+                'seanceByDay' => $seancesByDay,
+                'seance' => $seances,
+            ]);
+        }
 
 
     
