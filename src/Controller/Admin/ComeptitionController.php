0c5489533f632e2d7feb94242e543b3b3aad7d6d
siwar <siwar.maiti>
2023-04-26 12:53:21 +0200

    form

--- src/Controller/Admin/ComeptitionController.php
+++ src/Controller/Admin/ComeptitionController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller\admin;
+namespace App\Controller\Admin;
 use App\Entity\Competition;
 use App\Form\Admin\CompetitionType;
 use Doctrine\ORM\EntityManagerInterface;
--- src/Form/Admin/CompetitionType.php
+++ src/Form/Admin/CompetitionType.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Form;
+namespace App\Form\Admin;
 use App\Entity\competition;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
