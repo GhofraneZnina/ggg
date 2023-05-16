a9a11a6a812cf4b67c6fa5af09365149f5daf370
GhofraneZnina <ghofrane.znina@etudiant-isi.utm.tn>
2023-05-16 13:29:52 +0100

    statistiques

--- src/Controller/ChartController.php
+++ src/Controller/ChartController.php
@@ -10,6 +10,10 @@ use App\Repository\GroupeRepository;
 use App\Entity\User;
 use App\Repository\UserRepository;
 
+use App\Entity\Planning;
+use App\Repository\PlanningRepository;
+
+
 use App\Entity\Entraineur;
 use App\Repository\EntraineurRepository;
 
@@ -39,8 +43,9 @@ class ChartController extends AbstractController
             $repository = $entityManager->getRepository(Groupe::class);
             $groupes = $repository->findAll();
        
-      $users =$this->em->getRepository(User::class)->findAll();
-
+            $users = $this->em->getRepository(User::class)->findAll();
+            $plannings = $this->em->getRepository(Planning::class)->findAll();
+            
         $categNom = [];
         $categColor = [];
         $borderColor= [];
@@ -59,14 +64,44 @@ class ChartController extends AbstractController
        
          $categCount[] = count([0, 10, 5, 2, 20, 30, 45]);
         }
+        
+        //**** Begin Chart2 planningGraph planningsCount*/
+        $planningsNom = [];
+        $planningsColor = [];
+        $borderColor= [];
+
+        $planningsCount = [];
+
+     
+            foreach($plannings as $planning){
+         $planningsNom[] = $planning->getLabel();
+       
+        $planningsColor[] = [
+            'rgb(255, 99, 132)'
+        
+        ];
+        $borderColor[] ='rgb(255, 99, 132)';
+       
+         $planningsCount[] = count([0, 10, 5, 2, 20, 30, 45]);
+        }
      return $this->render('chart/index.html.twig', [
               'categNom' => json_encode($categNom),
               'categColor' => json_encode($categColor),
               'borderColor' => json_encode($borderColor),
               'categCount' => json_encode($categCount),
+
+              'planningsNom' => json_encode($planningsNom),
+              'planningsColor' => json_encode($planningsColor),
+              'borderColor1' => json_encode($borderColor),
+              'planningsCount' => json_encode($planningsCount),
         ]);
     }
 
    
 
 }
+
+
+
+
+
